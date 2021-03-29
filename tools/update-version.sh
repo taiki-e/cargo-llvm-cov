6283025bfabdea765e1396c3f5b924a54d52cdd8
Taiki Endo <te316e89@gmail.com>
2021-03-30 05:39:45 +0900

    Update tools/update-version.sh

--- tools/update-version.sh
+++ tools/update-version.sh
@@ -4,13 +4,13 @@
 #
 # Usage:
 #    ./tools/update-version.sh [options]
-#    ./tools/update-version.sh -y patch
-#    ./tools/update-version.sh -y minor
-#    ./tools/update-version.sh -y prerelease --pre-id alpha
+#    ./tools/update-version.sh patch
+#    ./tools/update-version.sh minor
+#    ./tools/update-version.sh prerelease --pre-id alpha
 #
 # Note: This script requires cargo-workspaces <https://github.com/pksunkara/cargo-workspaces>
 
 set -euo pipefail
 IFS=$'\n\t'
 
-cargo workspaces version --force '*' --no-git-commit --exact "$@"
+cargo workspaces version --force '*' --no-git-commit --exact -y "$@"
