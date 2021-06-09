dd85f85f87b61381c35a462d63810cfd421d4628
Taiki Endo <te316e89@gmail.com>
2021-06-09 22:38:11 +0900

    Add test for #21

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -3,7 +3,7 @@ mod auxiliary;
 use anyhow::Context as _;
 use auxiliary::cargo_llvm_cov;
 
-fn set(model: &str, name: &str) {
+fn run(model: &str, name: &str) {
     let id = format!("{}/{}", model, name);
     cargo_llvm_cov(model, name, "summary.txt", []).context(id.clone()).unwrap();
     cargo_llvm_cov(model, name, "json", ["--json", "--summary-only"]).context(id.clone()).unwrap();
@@ -16,9 +16,27 @@ fn set(model: &str, name: &str) {
 // It seems rustup is not installed in the docker image provided by cross.
 #[cfg_attr(target_env = "musl", ignore)]
 #[test]
-fn test() {
-    set("real1", "workspace_root");
-    set("virtual1", "workspace_root");
+fn real1_workspace_root() {
+    run("real1", "workspace_root");
+}
+
+// It seems rustup is not installed in the docker image provided by cross.
+#[cfg_attr(target_env = "musl", ignore)]
+#[test]
+fn virtual1_workspace_root() {
+    run("virtual1", "workspace_root");
+}
+
+// TODO: we should fix this: https://github.com/taiki-e/cargo-llvm-cov/issues/21
+// It seems rustup is not installed in the docker image provided by cross.
+#[cfg_attr(target_env = "musl", ignore)]
+#[test]
+fn no_test() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_test_writer()
+        .init();
+    run("no_test", "no_test");
 }
 
 // TODO:
---
+++ tests/fixtures/coverage/no_test/src/lib.rs
@@ -0,0 +1,5 @@
+mod module;
+pub use module::*;
+
+#[test]
+fn f() {}
