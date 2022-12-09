01d98f376eafbbf28ce3b0d5673589cb61da163b
Taiki Endo <te316e89@gmail.com>
2022-12-09 22:01:02 +0900

    Add spell-check to CI

--- tools/publish.sh
+++ tools/publish.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 IFS=$'\n\t'
 cd "$(dirname "$0")"/..
 
+# shellcheck disable=SC2154
+trap 's=$?; echo >&2 "$0: Error on line "${LINENO}": ${BASH_COMMAND}"; exit ${s}' ERR
+
 # Publish a new release.
 #
 # USAGE:
