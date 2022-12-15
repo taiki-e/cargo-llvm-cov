6cc8c1c43433b5c28dc771861264b3c7304b5e32
Taiki Endo <te316e89@gmail.com>
2022-12-15 20:44:28 +0900

    Set rerun-if-changed in test crate's build script

--- tests/fixtures/crates/ffi/build.rs
+++ tests/fixtures/crates/ffi/build.rs
@@ -1,4 +1,8 @@
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=hello_c.c");
+    println!("cargo:rerun-if-changed=hello_cpp.cpp");
+
     cc::Build::new().file("hello_c.c").compile("hello_c");
     cc::Build::new().cpp(true).file("hello_cpp.cpp").compile("libhello_cpp.a");
 }
