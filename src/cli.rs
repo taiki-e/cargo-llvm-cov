ad7abb9e5639b73ef824659076c93ef2269b19d6
TriplEight <denis.pisarev@parity.io>
2023-04-29 15:27:28 +0200

    also docline

--- src/cli.rs
+++ src/cli.rs
@@ -83,7 +83,7 @@ pub(crate) struct Args {
     // pub(crate) jobs: Option<u32>,
     /// Build artifacts in release mode, with optimizations
     pub(crate) release: bool,
-    /// Build artifacts with the specified profile
+    /// Nextest profile to use
     pub(crate) profile: Option<String>,
     // /// Space or comma separated list of features to activate
     // pub(crate) features: Vec<String>,
