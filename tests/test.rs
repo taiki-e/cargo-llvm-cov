9b93f70f4c5a06d6ee221d3537067bc8d3f5b7c6
Taiki Endo <te316e89@gmail.com>
2024-10-27 02:01:06 +0900

    Apply unqualified_local_imports lint

--- tests/test.rs
+++ tests/test.rs
@@ -6,11 +6,12 @@ mod auxiliary;
 
 use std::path::Path;
 
-use auxiliary::{
+use fs_err as fs;
+
+use self::auxiliary::{
     assert_output, cargo_llvm_cov, fixtures_path, normalize_output, perturb_one_header,
     test_project, test_report, CommandExt,
 };
-use fs_err as fs;
 
 const SUBCOMMANDS: &[&str] = &["", "run", "report", "clean", "show-env", "nextest"];
 
